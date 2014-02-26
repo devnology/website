39058c4307bbf74340d4fc278889581126a8af17
Arne Timmerman <arne@finalist.nl>
2014-02-26 08:13:19 +0100

    separate CSS and JS files for application and backend

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -1,3 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require bootstrap-datetimepicker.min
+
 $(function() {
   $(".form_datetime").datetimepicker({format: 'dd-mm-yyyy hh:ii'});
 

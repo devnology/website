9ff9bb54bf79c02b70515efe4d7467159878a641
Arne Timmerman <arne@finalist.nl>
2014-02-26 21:50:55 +0100

    first version of frontend page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
 
   def homepage
-    @upcoming_events = Event.where("end_time > ?", Time.now)
+    @upcoming_events = Event.where("end_time > ?", Time.now).order("end_time")
+    @next_event = @upcoming_events.first
   end
 
 end

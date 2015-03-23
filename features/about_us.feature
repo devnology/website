54a4a0d026fe047d8cbe1d07067549df3a9f83ad
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-23 20:35:18 +0100

    Create an About Us feature
    This feature is tagged as a work in progress as not to interfere with
    the continuous integration test

---
+++ features/about_us.feature
@@ -0,0 +1,7 @@
+Feature: about us
+
+  @wip
+  Scenario: Visit About Us
+    Given I visit the website
+    When I click the About Us page
+    Then I want to see About Us
---
+++ features/steps/about_us.rb
@@ -0,0 +1,12 @@
+class Spinach::Features::AboutUs < Spinach::FeatureSteps
+
+  step 'I visit the website' do
+  end
+
+  step 'I click the About Us page' do
+  end
+
+  step 'I want to see About Us' do
+  end
+
+end

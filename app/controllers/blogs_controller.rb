0acd7b1e164d6fcf8dbcd27eac54aa79f24d570c
Arne Timmerman <arne@xs4all.nl>
2015-09-05 14:54:05 +0200

    show only description of first blog on index page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,11 @@
 class BlogsController < ApplicationController
 
   def index
-    @blogs = Blog.all.order("created_at DESC")
+    blogs = Blog.all.order("created_at DESC")
+
+    @most_recent_blog = blogs.first
+    @old_blogs = blogs.drop(1)
+
     @recent_tweets = TwitterService.new.recent_tweets
   end
 

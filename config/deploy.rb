bea4a8f0ec1d309423afd0ae7ba057207ab6d65a
Arne Timmerman <arne@xs4all.nl>
2015-09-05 23:19:47 +0200

    upgrade to Ruby 2.2.3

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'git@github.com:devnology/website.git'
 set :deploy_to, '/home/deploy/website'
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.1'
+set :rbenv_ruby, '2.2.3'
 
 set :linked_files, %w{config/database.yml config/application.yml config/mail.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
